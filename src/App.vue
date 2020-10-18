b76c0bdbe33d133b4710ffdf225f88ae3face71f
Lucas Chen <lucas.chen.formal@gmail.com>
2020-10-18 21:49:09 +0800

    install router

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-    </v-app-bar>
-
-    <v-main>
-      <CustomCard/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/ziweiboard">紫微命盤</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import CustomCard from './components/CustomCard.vue';
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
+#nav {
+  padding: 30px;
 
-  components: {
-    CustomCard,
-  },
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-  data: () => ({
-    //
-  }),
-};
-</script>
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <CustomCard title="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CustomCard from '@/components/CustomCard.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    CustomCard
+  }
+}
+</script>
