abb86d666d2ba505ccd2c0bbdf85e9a52ebc5365
Lucas Chen <lucas.chen.formal@gmail.com>
2020-11-06 17:46:52 +0800

    setup vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentUser: {
+      gender: 0,
+      birthday: 0,
+      board: {}
+    }
   },
   mutations: {
+    changeGender(state, payload) {
+      state.currentUser.gender = payload.currentUser.gender
+    },
+    changeBirthday(state, payload) {
+      state.currentUser.birthday = payload.currentUser.birthday
+    },
+    changeBoard(state, payload){
+      state.currentUser.board = payload.currentUser.board
+    },
   },
   actions: {
+    setBoardInfo(state, payload) {
+      state.commit('changeGender', payload)
+      state.commit('changeBirthday', payload)
+      state.commit('changeBoard', payload)
+    }
   },
   modules: {
+  },
+  getters: {
+    board: state => {
+      return state.currentUser.board
+    }
   }
 })
